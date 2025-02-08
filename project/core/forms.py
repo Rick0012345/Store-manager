7a9b63c4bdc4989bb09e2a903e74d34572e83881
Patrick <po44243@gmail.com>
2025-02-07 21:17:03 -0300

    adicionei o campo de select dos tipos de produto

--- project/core/forms.py
+++ project/core/forms.py
@@ -1,12 +1,16 @@
 from django.forms import ModelForm
-from .models import Produto
+from .models import Produto, TipoProduto
+from django import forms
 
 class ProdutoForm(ModelForm):
+    tipo = forms.ChoiceField(choices=TipoProduto.TIPO_CHOICES)
+
     class Meta:
         model = Produto
-        fields = ['nome', 'preco']
+        fields = ['tipo', 'nome', 'preco']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         field = self.fields['preco']
         field.widget.attrs.update({'class': 'form-control form-preco'})
+
